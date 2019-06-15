3a249bea4c499e4f58ca1fa52c2baeab66716d07
Fcmam5 <dr.fcmam5@gmail.com>
2019-06-15 23:02:01 +0200

    feat($tools/crawlers/baladyats): sort and convert baladyats codes to Number()

--- tools/results-mapper/main.js
+++ tools/results-mapper/main.js
@@ -1,4 +1,25 @@
 /**
+ * This file is used to sort/clean and map results,
+ *    before moving the resulted file to the main results/ dir
  * The main WilayaList object was contructed, so we will use it from now :)
  * The code used for that is in tools/results-mapper/legacy/mapper.legacy.js
  */
+
+const fs = require('fs');
+
+// Sort baladyiats
+const WilayaList = require('./result.json');
+
+WilayaList.forEach((wilaya, wIndex) => {
+  wilaya.dairats.forEach((daira, dIndex) => {
+    if (daira.baladyiats && daira.baladyiats.length) {
+      // daira.baladyiats
+      const sortedBaladyats = daira.baladyiats
+        .map(bld => ({ ...bld, code: Number(bld.code) }))
+        .sort((a, b) => a.code - b.code);
+      WilayaList[wIndex].dairats[dIndex].baladyiats = sortedBaladyats;
+    }
+  });
+});
+
+fs.writeFileSync(`${__dirname}/result.json`, JSON.stringify(WilayaList));
