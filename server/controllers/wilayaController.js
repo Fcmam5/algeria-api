b1ed06b76ff7b03c79b7dbd0852479dd4dd867d1
Fcmam5 <dr.fcmam5@gmail.com>
2019-08-21 13:22:58 +0100

    feat($api): use mongoDB instead of the JSON file

--- server/controllers/wilayaController.js
+++ server/controllers/wilayaController.js
@@ -1,30 +1,52 @@
-// TODO: Replace file by DB when all fields are filled
-const WilayaList = require('../../results/WilayaList.json');
+const { Wilaya } = require('../models/index');
 const { getWilayasNames } = require('../utils');
 
 const wilayaController = {
-  list: (req, res) => res.status(200).json({ data: WilayaList }),
-  wilayaByMatricule: (req, res) => {
-    const matricule = Number(req.params.matricule);
-    const theWilaya = WilayaList[matricule - 1];
-    return res.status(200).json({ data: theWilaya });
+  list: async (req, res) => {
+    try {
+      const wilayas = await Wilaya.find({}, { _id: 0, __v: 0 }).exec();
+      return res.status(200).json({ data: wilayas });
+    } catch (error) {
+      return res.status(500).json({ msg: 'Error!' });
+    }
   },
-  adjacentWilayas: (req, res) => {
-    const matricule = Number(req.params.matricule);
-    const { adjacentWilayas } = WilayaList[matricule - 1];
-    return res.status(200).json({ data: adjacentWilayas });
+  wilayaByMatricule: async (req, res) => {
+    const mattricule = Number(req.params.matricule);
+    try {
+      const theWilaya = await Wilaya.findOne({ mattricule }, { _id: 0, __v: 0 }).exec();
+      return res.status(200).json({ data: theWilaya });
+    } catch (error) {
+      return res.status(500).json({ msg: 'Error!' });
+    }
   },
-  adjacentWilayasNames: (req, res) => {
-    const matricule = Number(req.params.matricule);
+  adjacentWilayas: async (req, res) => {
+    const mattricule = Number(req.params.matricule);
+    try {
+      const theWilaya = await Wilaya.findOne({ mattricule }, { _id: 0, __v: 0 }).exec();
+
+      const { adjacentWilayas } = theWilaya;
+      return res.status(200).json({ data: adjacentWilayas });
+    } catch (error) {
+      return res.status(500).json({ msg: 'Error!' });
+    }
+  },
+  adjacentWilayasNames: async (req, res) => {
+    const mattricule = Number(req.params.matricule);
     const { lang } = req.params;
-    const { adjacentWilayas } = WilayaList[matricule - 1];
-    const adjacentWilayasWithNames = getWilayasNames(adjacentWilayas, lang);
-    return res.status(200).json({
-      data: {
-        names: adjacentWilayasWithNames,
-        mattricules: adjacentWilayas,
-      },
-    });
+
+    try {
+      const theWilaya = await Wilaya.findOne({ mattricule }, { _id: 0, __v: 0 }).exec();
+      const { adjacentWilayas } = theWilaya;
+      const adjacentWilayasWithNames = getWilayasNames(adjacentWilayas, lang);
+      return res.status(200).json({
+        data: {
+          names: adjacentWilayasWithNames,
+          mattricules: adjacentWilayas,
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({ msg: 'Error!' });
+    }
   },
 };
 
