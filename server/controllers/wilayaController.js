74f6e45a17db9d7208ea6c017b901b82c497c502
Fcmam5 <dr.fcmam5@gmail.com>
2019-08-24 03:59:39 +0100

    feat($api): use xmlify

--- server/controllers/wilayaController.js
+++ server/controllers/wilayaController.js
@@ -1,24 +1,42 @@
+const xmlify = require('xmlify');
 const { Wilaya } = require('../models/index');
 const { getWilayasNames } = require('../utils');
 
 const wilayaController = {
   list: async (req, res) => {
+    const resFormat = req.query.format || 'json';
     try {
       const wilayas = await Wilaya.find({}, {
         _id: 0, __v: 0, 'dairats._id': 0, 'dairats.baladyiats._id': 0,
       }).exec();
-      return res.status(200).json({ data: wilayas });
+
+      switch (resFormat) {
+        case 'xml':
+          res.type('application/xml');
+          return res.send(xmlify(wilayas, 'wilayas'));
+        default:
+          return res.status(200).json({ data: wilayas });
+      }
     } catch (error) {
       return res.status(500).json({ msg: 'Error!' });
     }
   },
   wilayaByMatricule: async (req, res) => {
+    const resFormat = req.query.format || 'json';
     const mattricule = Number(req.params.matricule);
+
     try {
       const theWilaya = await Wilaya.findOne({ mattricule }, {
         _id: 0, 'dairats._id': 0, 'dairats.baladyiats._id': 0, __v: 0,
       }).exec();
-      return res.status(200).jsonp({ data: theWilaya });
+
+      switch (resFormat) {
+        case 'xml':
+          res.type('application/xml');
+          return res.send(xmlify(theWilaya, 'wilaya'));
+        default:
+          return res.status(200).jsonp({ data: theWilaya });
+      }
     } catch (error) {
       return res.status(500).json({ msg: 'Error!' });
     }
