acaa9c8ace97dfe185de541144d3fa0a4a2c76ab
Fcmam5 <dr.fcmam5@gmail.com>
2019-11-04 22:48:58 +0100

    feat($api): use Joi package

--- server/validations/WilayaValidator.js
+++ server/validations/WilayaValidator.js
@@ -1,31 +1,33 @@
 
-const joi = require('joi');
+const Joi = require('joi');
 
-const createWilayaSchema = {
-  mattricule: joi.number().min(1).max(48),
-  name: joi.string().required(),
-  nameAr: joi.string().required(),
-  nameEn: joi.string().required(),
-  phoneCodes: joi.array().items(joi.number()),
-  postalCodes: joi.array().items(joi.number()),
-  dairats: [{
-    code: joi.number().required(),
-    name: joi.string().required(),
-    nameAr: joi.string().required(),
-    nameEn: joi.string().required(),
-    baladyiats: [{
-      code: joi.number().required(),
-      name: joi.string().required(),
-      nameAr: joi.string().required(),
-      nameEn: joi.string().required(),
-    }],
-  }],
-  adjacentWilayas: joi.array().items(joi.number().min(1).max(48)),
-};
+// const createWilayaSchema = {
+//   mattricule: Joi.number().min(1).max(48),
+//   name: Joi.string().required(),
+//   nameAr: Joi.string().required(),
+//   nameEn: Joi.string().required(),
+//   phoneCodes: Joi.array().items(Joi.number()),
+//   postalCodes: Joi.array().items(Joi.number()),
+//   dairats: [{
+//     code: Joi.number().required(),
+//     name: Joi.string().required(),
+//     nameAr: Joi.string().required(),
+//     nameEn: Joi.string().required(),
+//     baladyiats: [{
+//       code: Joi.number().required(),
+//       name: Joi.string().required(),
+//       nameAr: Joi.string().required(),
+//       nameEn: Joi.string().required(),
+//     }],
+//   }],
+//   adjacentWilayas: Joi.array().items(Joi.number().min(1).max(48)),
+// };
 
+const wilayaMatriculeSchema = Joi.number().min(1).max(48);
 
 const WilayaValidator = {
-  create: joi.object().keys(createWilayaSchema).required(),
+  // create: Joi.object().keys(createWilayaSchema).required(),
+  isInWilayasRange: matricule => Joi.validate(matricule, wilayaMatriculeSchema),
 };
 
 module.exports = WilayaValidator;
