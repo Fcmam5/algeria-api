702114c02cd56fe7122408415346ecbd492fe1b9
Fcmam5 <dr.fcmam5@gmail.com>
2019-11-05 22:13:18 +0100

    fix test

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -3,9 +3,9 @@ const { getServer } = require('../../test-utils');
 const server = getServer();
 
 describe('Catch 404 and forward to error handler', () => {
-  it('should return 404', () => {
+  it('should return 404', (done) => {
     server
       .get('/completely-random-url')
-      .expect(404);
+      .expect(404, done);
   });
 });
